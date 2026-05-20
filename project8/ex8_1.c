7ed825cbb017e32a264e1ca10f354539402ba1fb
Yoonseok Choi <jsnoddy1@cu.ac.kr>
2026-05-20 15:30:41 +0900

    rename project7 temp

--- project8/ex8_1.c
+++ project8/ex8_1.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "adjMatrix.h"
 
 int main(void){
@@ -10,7 +11,7 @@ int main(void){
     
     createGraph(G1);
 	for (i = 0; i < 4; i++)
-		insertVertex(G1, i);	//G1의 정점 0~3 삽입
+		insertVertex(G1, i);	
 	insertEdge(G1, 0, 3);
 	insertEdge(G1, 0, 1);
 	insertEdge(G1, 1, 3);
@@ -22,10 +23,9 @@ int main(void){
 	insertEdge(G1, 3, 1);
 	insertEdge(G1, 3, 0);
 
-	//G2 구성 : 정점 u에 대한 간선 (u,v)의 삽입순서는 v가 큰 것부터.
 	createGraph(G2);
 	for (i = 0; i < 3; i++)
-		insertVertex(G2, i);	// G2의 정점 0~2 삽입
+		insertVertex(G2, i);	
 	insertEdge(G2, 0, 2);
 	insertEdge(G2, 0, 1);
 	insertEdge(G2, 1, 2);
@@ -33,26 +33,23 @@ int main(void){
 	insertEdge(G2, 2, 1);
 	insertEdge(G2, 2, 0);
 
-	//G3 구성 : 정점 u에 대한 간선 <u,v>의 삽입순서는 v가 큰 것부터.
 	createGraph(G3);
 	for (i = 0; i < 4; i++)
-		insertVertex(G3, i);	// G3의 정점 0~3 삽입
+		insertVertex(G3, i);	
 	insertEdge(G3, 0, 3);
 	insertEdge(G3, 0, 1);
 	insertEdge(G3, 1, 3);
 	insertEdge(G3, 1, 2);
 	insertEdge(G3, 2, 3);
 
-	//G4 구성 : 정점 u에 대한 간선 <u,v>의 삽입순서는 v가 큰 것부터.
 	createGraph(G4);
 	for (i = 0; i < 3; i++)
-		insertVertex(G4, i);	// G4의 정점 0~2 삽입
+		insertVertex(G4, i);	
 	insertEdge(G4, 0, 2);
 	insertEdge(G4, 0, 1);
 	insertEdge(G4, 1, 2);
 	insertEdge(G4, 1, 0);
 	
-	//--- 완성된 인접 행렬 출력
 	printf("\n G1의 인접 행렬");
 	print_adjMatrix(G1);
 
